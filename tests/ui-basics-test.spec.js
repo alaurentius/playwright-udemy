b977faaba4146101609015b2440828becd019d0d
nglcode <nglcode@mailbox.org>
2025-09-07 22:44:24 +0200

    Refactor browser context test to improve locator usage and assertions

--- tests/ui-basics-test.spec.js
+++ tests/ui-basics-test.spec.js
@@ -5,14 +5,23 @@ test.only('Browser Context Playwright Test', async ({ browser }) => {
     // These can be defined, or passed in as fixtures and it will use default config
     const context = await browser.newContext();
     const page = await context.newPage();
+    const userName = page.locator('#username')
+    const password = page.locator('[type="password"]')
+    const signInBtn = page.locator('#signInBtn')
+
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
     console.log(await page.title());
     await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
-    await page.locator('#username').fill('rahulshetty');
-    await page.locator('[type="password"]').fill('learning');
-    await page.locator('#signInBtn').click();
+    await userName.fill('rahulshetty');
+    await password.fill('learning');
+    await signInBtn.click();
     console.log(await page.locator('[style*="block"]').textContent());
-    await expect(page.locator('[style*="block"]').textContent() ).toContain('Incorrect username/password.');
+    await expect(page.locator('[style*="block"]')).toContainText('Incorrect username/password.');
+    await userName.fill('');
+    await userName.fill('rahulshettyacademy');
+    await signInBtn.click();
+    console.log(await page.locator('.card-body a').first().textContent());
+    console.log(await page.locator('.card-body a').last().textContent());
 });
 
 test('Page Playwright Test', async ({ page }) => {
