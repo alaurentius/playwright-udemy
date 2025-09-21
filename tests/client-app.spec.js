b21eee3e5bd00019abba7aed634b7521b3b56917
nglcode <nglcode@mailbox.org>
2025-09-21 22:34:10 +0200

    fix: update Client App login test with correct credentials and add product cart functionality

--- tests/client-app.spec.js
+++ tests/client-app.spec.js
@@ -1,20 +1,39 @@
 const { test, expect } = require('@playwright/test');
 
-test('Dynamically Waits Test', async ({ page }) => {
+test.only('Client App login', async ({ page }) => {
 
     await page.goto('https://rahulshettyacademy.com/client');
     const userName = page.locator('#userEmail')
     const password = page.locator('#userPassword')
     const loginBtn = page.locator('[value="Login"]')
+    const cartBtn = page.locator('[routerlink*="cart"]')
+    const products = page.locator('.card-body')
     const cardTitles = page.locator('.card-body b')
+    const cartItems = page.locator('div.cart li')
+    const productName = 'ZARA COAT 3';
 
-    await userName.fill('anshika@gmail.com');
-    await password.fill('Iamking@000');
+
+    await userName.fill('1RrKgVZUdVko@yopmail.com');
+    await password.fill('1RrKgVZUdVko');
     await loginBtn.click();
     // await page.waitForLoadState('networkidle'); // discouraged in playwright docs
     // await cardTitles.waitFor(); // works only for single element
     await cardTitles.last().waitFor();
     const titles = await cardTitles.allTextContents();
     console.log(titles);
+    const count = await products.count();
+
+    for (let i = 0; i < count; ++i) {
+        if (await cardTitles.nth(i).textContent() === productName) {
+            // add to cart
+            await products.nth(i).locator('text= Add To Cart').click();
+            break;
+        }
+    }
+    await cartBtn.click();
+    await cartItems.last().waitFor();
+    const isProductVisible = await page.locator('h3:has-text("' + productName + '")').isVisible();
+    expect(isProductVisible).toBeTruthy();
+    
 });
 
