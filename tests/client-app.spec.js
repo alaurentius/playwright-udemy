cb479dcf9d2e7bc8d6760f0aad6e01d6d9b4bca2
nglcode <nglcode@mailbox.org>
2025-09-21 22:53:00 +0200

    add: enhance Client App login test with checkout and country selection functionality

--- tests/client-app.spec.js
+++ tests/client-app.spec.js
@@ -11,7 +11,11 @@ test.only('Client App login', async ({ page }) => {
     const cardTitles = page.locator('.card-body b')
     const cartItems = page.locator('div.cart li')
     const productName = 'ZARA COAT 3';
-
+    const checkoutBtn = page.locator('text="Checkout"');
+    const countryInput = page.locator('[placeholder*="Country"]');
+    const countryResults = page.locator('.ta-results');
+    const countryBtns = countryResults.locator('button');
+    
 
     await userName.fill('1RrKgVZUdVko@yopmail.com');
     await password.fill('1RrKgVZUdVko');
@@ -34,6 +38,14 @@ test.only('Client App login', async ({ page }) => {
     await cartItems.last().waitFor();
     const isProductVisible = await page.locator('h3:has-text("' + productName + '")').isVisible();
     expect(isProductVisible).toBeTruthy();
-    
+    await checkoutBtn.click();
+    await countryInput.pressSequentially('ind', { delay: 150 });
+    await countryResults.waitFor();
+    const optionsCount = await countryBtns.count();
+    for (let i = 0; i < optionsCount; ++i) {
+        if (await countryBtns.nth(i).textContent() === ' India') {
+            await countryBtns.nth(i).click();
+            break;
+        }
+    }
 });
-
