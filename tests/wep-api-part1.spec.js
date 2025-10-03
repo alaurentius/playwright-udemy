29797427c34fce7565adb7a15f4df1c532b4bb74
nglcode <nglcode@mailbox.org>
2025-10-04 01:05:11 +0200

    add: implement APIUtils class for handling authentication and order creation

--- tests/wep-api-part1.spec.js
+++ tests/wep-api-part1.spec.js
@@ -1,44 +1,42 @@
 const { test, expect, request } = require('@playwright/test');
+const {APIUtils} = require('./utils/api-utils')
 
 const loginPayload = { userEmail: "1RrKgVZUdVko@yopmail.com", userPassword: "1RrKgVZUdVko" }
-let token;
+const createOrderPayload = { orders: [{ country: "Cuba", productOrderedId: "68a961459320a140fe1ca57a" }] }
+let response
 
 test.beforeAll(async () => {
-
     const apiContext = await request.newContext()
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", {
-        data: loginPayload
-    })
-    expect(loginResponse.ok()).toBeTruthy()
-    const loginResponseJson = await loginResponse.json()
-    token = loginResponseJson.token;
-    console.log(token)
+    const apiUtils = new APIUtils(apiContext, loginPayload)
+    response = await apiUtils.createOrder(createOrderPayload);
 })
 
-test.beforeEach(() => {
-
-})
-
-test('Client App login - API', async ({ page }) => {
+test('Client App login - API', async ({ page }) => { 
 
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value)
-    }, token)
+    }, response.token)
 
-    const productName = 'ZARA COAT 3';
+    const ordersBtn = page.locator('button[routerlink*="myorders"]')
+    const tableBody = page.locator('tbody');
+    const ordersIds = page.locator('tbody tr th');
+    const orderIdSummary = page.locator('.col-text');
+    const viewBtns = page.locator('tbody tr td button.btn-primary');
+    
     await page.goto("https://rahulshettyacademy.com/client");
 
-    await page.locator(".card-body b").first().waitFor();
-    await page.locator(".card-body").filter({ hasText: productName })
-        .getByRole("button", { name: "Add to Cart" }).click();
-    await page.getByRole("listitem").getByRole('button', { name: "Cart" }).click();
+    await ordersBtn.click();
+    await tableBody.waitFor();
 
-    //await page.pause();
-    await page.locator("div li").first().waitFor();
-    await expect(page.getByText(productName)).toBeVisible();
-    await page.getByRole("button", { name: "Checkout" }).click();
-    await page.getByPlaceholder("Select Country").pressSequentially("ind");
-    await page.getByRole("button", { name: "India" }).nth(1).click();
-    await page.getByText("PLACE ORDER").click();
-    await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+    for (let i = 0; i < await ordersIds.count(); ++i) {
+        const rowOrderIdText = await ordersIds.nth(i).textContent();
+        console.log(rowOrderIdText);
+        if (response.orderId.includes(rowOrderIdText)) {
+            await viewBtns.nth(i).click();
+            break;
+        }
+    }
+    const orderIdSummaryText = await orderIdSummary.textContent();
+    expect(response.orderId.includes(orderIdSummaryText)).toBeTruthy();
 })
+
